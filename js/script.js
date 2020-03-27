64183cd89ae56943ec2f3c6ec134e17180e5e359
Marcelina <marcelina.hasiak@gmail.com>
2020-03-27 12:26:03 +0000

    Merged in feature/glitch-animation (pull request #2)
    Feature/glitch animation
    
    * Glitch animation on h1
    
    * Change-bg-fixed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
-$(document).ready(function() {
-    $("svg.main-nav__element__icon").hover(function() {
-        $(this).siblings("div").addClass("main-nav__element__bg-1");
-    }, function() {
-        $(this).siblings("div").removeClass("main-nav__element__bg-1");
-    });
-  });
+
+
+    // const backgroundImages = [
+    //   "1.jpg",
+    //   "2.jpg",
+    //   "3.jpg",
+    //   "4.jpg",
+    // ];
+
+    // base = "../img/bg-";
+    // backgroundImages.forEach(function(img){
+    //   new Image().src = base + img;
+    // });
   
+    // let count = 1;
+   
+    // $(".site-wrapper").css("background-image", 'url("' + base + backgroundImages[0] + '")');
+
+    // setInterval(function() {
+    //   $(".site-wrapper").css("background-image", 'url("' + base + backgroundImages[count] + '")');
+    //   count === backgroundImages.length - 1 ? (count = 0) : count++;
+    // }, 8000);
+
+
+/*var images = new Array()
+
+function preload() {
+  for (i = 0; i < preload.arguments.length; i++) {
+    images[i] = new Image()
+    images[i].src = preload.arguments[i]
+  }
+}
+preload(
+  "../img/bg-1.jpg",
+  "../img/bg-2.jpg",
+  "../img/bg-3.jpg",
+  "../img/bg-4.jpg",
+  "../img/bg-5.jpg"
+)
+
+      let count = 1;
+   
+      $(".site-wrapper").css("background-image", 'url("' + images[0] + '")');
+      setInterval(function() {
+        
+        $(".site-wrapper").css("background-image", 'url("' + images[count] + '")');
+        count === images.length - 1 ? (count = 0) : count++;
+          
+      }, 8000);
+      */
